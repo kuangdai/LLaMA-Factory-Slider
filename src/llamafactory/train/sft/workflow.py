5f34600365e3f2c0f0d4a05fb2a73fb815431d45
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-21 18:09:48 +0000

    Update workflow.py

--- src/llamafactory/train/sft/workflow.py
+++ src/llamafactory/train/sft/workflow.py
@@ -51,6 +51,12 @@ def run_sft(
     dataset_module = get_dataset(template, model_args, data_args, training_args, stage="sft", **tokenizer_module)
     model = load_model(tokenizer, model_args, finetuning_args, training_args.do_train)
 
+    ####################
+    # SLIDER TOKENIZER #
+    ####################
+    if model_args.slider_on:
+        model.tokenizer = tokenizer
+
     if getattr(model, "is_quantized", False) and not training_args.do_train:
         setattr(model, "_hf_peft_config_loaded", True)  # hack here: make model compatible with prediction
 
