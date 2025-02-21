abe2582607a6dd904a3ecfabf59080274cad02dd
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-21 13:49:13 +0000

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/kuangdai/transformers-slider
+git clone https://github.com/kuangdai/LLaMA-Factory-Slider
+
+conda create -n slider python=3.10
+conda activate slider
+cd LLaMA-Factory-Slider
+pip install -r requirements.txt
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install deepspeed
+
+cd ../transformers-slider
+pip install -e .
