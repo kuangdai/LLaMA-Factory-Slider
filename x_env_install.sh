3ace66fc6cca1377da913167bee6a4124d909854
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 13:54:11 +0000

    Update x_env_install.sh

--- x_env_install.sh
+++ x_env_install.sh
@@ -1,9 +1,11 @@
+mkdir sd_slider
+cd sd_slider
 git clone https://github.com/kuangdai/LLaMA-Factory-Slider
 git clone https://github.com/kuangdai/transformers-slider
 git clone https://github.com/kuangdai/vllm-slider
 
-conda create -n slider python=3.10
-conda activate slider
+conda create -n sd_slider python=3.10 -y
+conda activate sd_slider
 cd LLaMA-Factory-Slider
 pip install -r requirements.txt
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
