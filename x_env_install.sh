26b81342a6410ecdab00c84c35174ce7625caec1
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2025-02-23 20:21:12 +0000

    Update x_env_install.sh

--- x_env_install.sh
+++ x_env_install.sh
@@ -1,26 +1,34 @@
+# Git code
 mkdir sd_slider
 cd sd_slider
 git clone https://github.com/kuangdai/LLaMA-Factory-Slider
 git clone https://github.com/kuangdai/transformers-slider
 git clone https://github.com/kuangdai/vllm-slider
 
+# Create env
 conda create -n sd_slider python=3.10 -y
 conda activate sd_slider
 
+# Install torch and deepspeed
 conda install -c nvidia cuda-toolkit=12.2 cudnn -y
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install deepspeed -i https://mirrors.aliyun.com/pypi/simple
 
+# Install LLaMA Factory
 cd LLaMA-Factory-Slider
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
-
-cd ../transformers-slider
-pip install -e .
-
-# Local installation of vllm is complicated by build wheel
-# We install latest pre-built and replace changed python files
 cd ..
+
+# Install transformers
+cd transformers-slider
+pip install -e .
+cd ..
+
+# Install vllm
+# Building from source is complicated.
+# We install the latest pre-built version and replace changed python files
 pip install vllm -U -i https://mirrors.aliyun.com/pypi/simple
+# You cannot run this in vllm-slide, or VLLM_DIR will be wrong
 export VLLM_DIR=$(dirname $(python -c "import vllm; print(vllm.__file__)"))
 cp vllm-slider/vllm/model_executor/models/qwen2.py $VLLM_DIR/model_executor/models/qwen2.py
 cp vllm-slider/vllm/model_executor/models/slider.py $VLLM_DIR/model_executor/models/slider.py
