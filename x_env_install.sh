85eb1138945ce70598e887aaa15bc3dcd864d50b
Kuangdai <kuangdai.leng@gmail.com>
2025-06-02 21:04:39 +0100

    Update x_env_install.sh

--- x_env_install.sh
+++ x_env_install.sh
@@ -1,13 +1,13 @@
+# Create env
+# conda create -n sd_slider python=3.10 -y
+# conda activate sd_slider
+
 # Git code
 mkdir sd_slider
 cd sd_slider
 git clone https://github.com/kuangdai/LLaMA-Factory-Slider
 git clone https://github.com/kuangdai/transformers-slider
 git clone https://github.com/kuangdai/vllm-slider
-
-# Create env
-conda create -n sd_slider python=3.10 -y
-conda activate sd_slider
 
 # Install torch and deepspeed
 conda install -c nvidia cuda-toolkit=12.2 cudnn -y
@@ -34,6 +34,8 @@ pip install vllm==0.7.3 -i https://mirrors.aliyun.com/pypi/simple
 # You cannot run this in vllm-slide, or VLLM_DIR will be wrong
 export VLLM_DIR=$(dirname $(python -c "import vllm; print(vllm.__file__)"))
 cp vllm-slider/vllm/model_executor/models/qwen2.py $VLLM_DIR/model_executor/models/qwen2.py
+cp vllm-slider/vllm/model_executor/models/qwen3.py $VLLM_DIR/model_executor/models/qwen3.py
+cp vllm-slider/vllm/model_executor/models/registry.py $VLLM_DIR/model_executor/models/registry.py
 cp vllm-slider/vllm/model_executor/models/slider.py $VLLM_DIR/model_executor/models/slider.py
 cp vllm-slider/vllm/model_executor/model_loader/loader.py $VLLM_DIR/model_executor/model_loader/loader.py
 cp vllm-slider/vllm/worker/model_runner.py $VLLM_DIR/worker/model_runner.py
